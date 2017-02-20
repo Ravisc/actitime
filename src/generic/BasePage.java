7afed535de60fb78d28ec5475992a1422144408b
Ravisc <s.c.ravikumar@gmail.com>
2017-02-20 16:55:10 +0530

    Uploading framework to git hub

---
+++ src/generic/BasePage.java
@@ -0,0 +1,29 @@
+package generic;
+
+import org.openqa.selenium.TimeoutException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.Reporter;
+
+public abstract class BasePage {
+ public WebDriver driver;
+  public void Basepage(WebDriver driver){
+	  this.driver=driver;
+	  PageFactory.initElements(driver, this);
+  }
+	
+  public void VerifyTitle(String etitle){
+	  WebDriverWait wait=new WebDriverWait(driver,10);
+	  try{
+	  wait.until(ExpectedConditions.titleIs(etitle));
+	  Reporter.log("Expected title is displayed");
+	  }
+	  catch(TimeoutException e){
+		  Reporter.log("Expected title is not displayed");
+		  Assert.fail();
+		  	  }
+  }
+}
